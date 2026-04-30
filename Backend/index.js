594402711e4c55fb07e03ef1fd95ece1c8dfdc90
SriSa <shtynidhi@gmail.com>
2026-04-30 20:01:05 +0530

    UPDATION

--- Backend/index.js
+++ Backend/index.js
@@ -23,10 +23,15 @@ mongoose.connect(process.env.MONGO_URL,)
 .catch((err) => console.log(err));
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
